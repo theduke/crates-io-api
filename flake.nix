1e69e3b2cd41ba92cd6822e0022dddf32036f35d
Christoph Herzog <chris@theduke.at>
2021-05-18 16:09:51 +0200

    build: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "omni-csi";
+
+  inputs = {
+    flakeutils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flakeutils }: 
+    flakeutils.lib.eachDefaultSystem (system:
+      let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in rec {
+
+        devShell = pkgs.stdenv.mkDerivation {
+            name = "crates-io-api";
+            src = self;
+            buildInputs = with pkgs; [
+              pkgconfig
+              openssl
+            ];
+            propagatedBuildInputs = with pkgs; [
+              openssl
+            ];
+            buildPhase = "";
+            installPhase = "";
+
+            RUST_BACKTRACE = "1";
+            RUSTFLAGS = "-C link-arg=-fuse-ld=lld";
+            LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib";
+        };
+
+      }
+    );
+}  
