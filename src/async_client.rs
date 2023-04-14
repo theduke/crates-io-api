a9a689b4b81a0ceb1df6d6ad984b4a20342c2630
Emil Jonathan Eriksson <eje1999@gmail.com>
2023-04-14 20:59:18 +0200

    Add recent_downloads field to FullCrate

--- src/async_client.rs
+++ src/async_client.rs
@@ -368,6 +368,7 @@ impl Client {
                     homepage: data.homepage,
                     repository: data.repository,
                     total_downloads: data.downloads,
+                    recent_downloads: data.recent_downloads,
                     max_version: data.max_version,
                     max_stable_version: data.max_stable_version,
                     created_at: data.created_at,
